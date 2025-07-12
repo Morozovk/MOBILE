4616ad3497182e85974991f90ba666322b5e9356
Morozov Kirill <k.morozov@magdv.com>
2025-07-12 04:14:29 +0300

    "Поменял название теста"

--- src/test/java/tests/emulation/OnbordingTest.java
+++ src/test/java/tests/emulation/OnbordingTest.java
@@ -11,7 +11,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 
-
 public class OnbordingTest extends TestBase {
 
     @Test
